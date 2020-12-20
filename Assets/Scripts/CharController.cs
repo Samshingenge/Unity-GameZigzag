eafc07b02f0a06ab53318b37ce7b692b825ff935
Sam <samblogshingenge@gmail.com>
2020-12-20 10:44:08 +0200

    My unity zigzag game

---
+++ Assets/Scripts/CharController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharController : MonoBehaviour
+{
+
+    public Transform rayStart;
+    public GameObject crystalEffect;
+
+    public Rigidbody rb;
+    private bool walkingRigth = true;
+    private Animator anim;
+    private GameManager gameManager;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (!gameManager.gameStarted)
+        {
+            return;
+        }
+        else
+        {
+            anim.SetTrigger("gameStarted");
+        }
+        rb.transform.position = transform.position + transform.forward * 2 * Time.deltaTime; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Switch();
+        }
+        RaycastHit hit;
+        if(!Physics.Raycast(rayStart.position,-transform.up, out hit, Mathf.Infinity))
+        {
+            anim.SetTrigger("isFalling");
+            Debug.Log("Falling");
+        }
+        else
+        {
+            anim.SetTrigger("notFallingAnymore");
+        }
+        if(transform.position.y < -2)
+        {
+            gameManager.EndGame();
+        }
+    }
+
+    private void Switch()
+    {
+
+        if (!gameManager.gameStarted)
+        {
+            return;
+        }
+        walkingRigth = !walkingRigth;
+        if (walkingRigth)
+        
+            transform.rotation = Quaternion.Euler(0, 45, 0);
+        
+        else
+        
+            transform.rotation = Quaternion.Euler(0, -45, 0);
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Crystal")
+        {
+            Destroy(other.gameObject);
+            gameManager.IncreaseScore();
+
+            GameObject g = Instantiate(crystalEffect, rayStart.transform.position, Quaternion.identity);
+            Destroy(g, 2);
+            Destroy(other.gameObject);
+        }
+    }
+
+}
